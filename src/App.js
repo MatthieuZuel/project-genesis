b7540bab839930515d886d27d2dc6654ae19504d
Matthieu Zuel <zuel_matthieu034@outlook.com>
2021-11-24 17:52:56 +0400

    add contact form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ContactPage, Home, PageNotFound } from './Pages';
 import GlobalStyle from './globalStyles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,11 +7,12 @@ function App() {
   return (
     <Router>
       <GlobalStyle/>
-      <Routes>
+      <ContactPage />
+      {/* <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/contact" exact element={ <ContactPage /> } />
         <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      </Routes> */}
     </Router>
   );
 }
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios'
 import {
     Form,
     FormButton,
@@ -9,32 +10,54 @@ import {
 } from './Contact.element'
 
 const Contact = () => {
+
+    const [sent, setSent] = useState(false)
+
+    const [text, setText] = useState("")
+
+    const handleSend = async() => {
+        setSent(true)
+        try {
+            await axios.post("http://localhost:4000/send_mail", {
+                text
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
             <Icon to="/"> Genesis Marketing </Icon>
-            <Form onSubmit="submit" >
-                <FormLabel>
-                    Name:
-                    <FormInput 
-                        type="text"
-                        placeholder="Your Name"
-                    />
-                </FormLabel>
-                <FormLabel>
-                    Email:
-                    <FormInput 
-                        type="email"
-                        placeholder="Your Email"
-                    />
-                </FormLabel>
-                <FormLabel>
-                    Message:
-                    <TextArea
-                        placeholder="Message"
-                    ></TextArea>
-                </FormLabel>
-                <FormButton type="submit"> Send </FormButton>
-            </Form>
+            {!sent ? (
+                <Form onSubmit={handleSend}>
+                    <FormLabel>
+                        <FormInput 
+                            type="text"
+                            value={text}
+                            onChange={(e) => 
+                                setText(e.target.value)
+                            }
+                        />
+                    </FormLabel>
+                    {/* <FormLabel>
+                        Email:
+                        <FormInput 
+                            type="email"
+                            placeholder="Your Email"
+                        />
+                    </FormLabel>
+                    <FormLabel>
+                        Message:
+                        <TextArea
+                            placeholder="Message"
+                        ></TextArea>
+                    </FormLabel> */}
+                    <FormButton type="submit"> Send </FormButton>
+                </Form>
+            ) : (
+                <h1>Email Sent</h1>
+            )}
         </>
     )
 }
