a7e778880e6b07feeb0d89637ae4410e5b89402c
Matthieu Zuel <zuel_matthieu034@outlook.com>
2022-01-11 21:28:59 +0400

    logo changed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,101 +1,104 @@
-import React, { useState, useEffect } from 'react'
-import { FaBars } from 'react-icons/fa'
-import {IconContext} from 'react-icons/lib'
-import { animateScroll as scroll } from 'react-scroll'
-import logo from '../../Images/Genesis Logo-01.svg'
+import React, { useState, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
+import logo from '../../Images/Genesis-final-design-logo.png';
 import {
-    Nav,
-    NavbarContainer,
-    NavLogo,
-    MobileIcon,
-    NavMenu,
-    NavItem,
-    NavLinks,
-    NavBtn,
-    NavBtnLink
-} from './Navbar.elements'
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	MobileIcon,
+	NavMenu,
+	NavItem,
+	NavLinks,
+	NavBtn,
+	NavBtnLink,
+} from './Navbar.elements';
 
 const Navbar = ({ toggle }) => {
+	const [scrollNav, setScrollNav] = useState(false);
 
-    const [scrollNav, setScrollNav] = useState(false)
+	const changeNav = () => {
+		if (window.scrollY >= 80) {
+			setScrollNav(true);
+		} else {
+			setScrollNav(false);
+		}
+	};
 
-    const changeNav = () => {
-        if (window.scrollY >= 80) {
-            setScrollNav(true)
-        } else {
-            setScrollNav(false)
-        }
-    }
+	useEffect(() => {
+		window.addEventListener('scroll', changeNav);
+	}, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', changeNav)
-    }, [])
+	const toggleHome = () => {
+		scroll.scrollToTop();
+	};
 
-    const toggleHome = () => {
-        scroll.scrollToTop();
-    }
+	return (
+		<>
+			<IconContext.Provider value={{ color: '#fff' }}>
+				<Nav scrollNav={scrollNav}>
+					<NavbarContainer>
+						<NavLogo to='/' onClick={toggleHome}>
+							<img src={logo} alt='logo' />
+						</NavLogo>
+						<MobileIcon onClick={toggle}>
+							<FaBars />
+						</MobileIcon>
+						<NavMenu>
+							<NavItem>
+								<NavLinks
+									to='about'
+									smooth={true}
+									duration={500}
+									spy={true}
+									exact='true'
+									offset={-80}>
+									Qui sommes nous ?
+								</NavLinks>
+							</NavItem>
+							<NavItem>
+								<NavLinks
+									to='action'
+									smooth={true}
+									duration={500}
+									spy={true}
+									exact='true'
+									offset={-80}>
+									Champ d'action
+								</NavLinks>
+							</NavItem>
+							<NavItem>
+								<NavLinks
+									to='services'
+									smooth={true}
+									duration={500}
+									spy={true}
+									exact='true'
+									offset={-80}>
+									Nos services
+								</NavLinks>
+							</NavItem>
+							<NavItem>
+								<NavLinks
+									to='discover'
+									smooth={true}
+									duration={500}
+									spy={true}
+									exact='true'
+									offset={-80}>
+									Notre expertise
+								</NavLinks>
+							</NavItem>
+						</NavMenu>
+						<NavBtn>
+							<NavBtnLink to='/contact'>Nous Contacter</NavBtnLink>
+						</NavBtn>
+					</NavbarContainer>
+				</Nav>
+			</IconContext.Provider>
+		</>
+	);
+};
 
-    return (
-        <>
-            <IconContext.Provider value={{color: '#fff'}}>
-                <Nav scrollNav={scrollNav}>
-                    <NavbarContainer>
-                        <NavLogo to='/' onClick={toggleHome}>
-                            <img src={logo} alt="logo" />
-                        </NavLogo>
-                        <MobileIcon onClick={toggle}>
-                            <FaBars />
-                        </MobileIcon>
-                        <NavMenu>
-                            <NavItem>
-                                <NavLinks
-                                    to="about"
-                                    smooth={true}
-                                    duration={500}
-                                    spy={true}
-                                    exact='true'
-                                    offset={-80}
-                                >Qui sommes nous ?</NavLinks>
-                            </NavItem>
-                            <NavItem>
-                                <NavLinks
-                                    to="action"
-                                    smooth={true}
-                                    duration={500}
-                                    spy={true}
-                                    exact='true'
-                                    offset={-80}
-                                >Champ d'action</NavLinks>
-                            </NavItem>
-                            <NavItem>
-                                <NavLinks
-                                    to="services"
-                                    smooth={true}
-                                    duration={500}
-                                    spy={true}
-                                    exact='true'
-                                    offset={-80}
-                                >Nos services</NavLinks>
-                            </NavItem>
-                            <NavItem>
-                                <NavLinks
-                                    to="discover"
-                                    smooth={true}
-                                    duration={500}
-                                    spy={true}
-                                    exact='true'
-                                    offset={-80}
-                                >Notre expertise</NavLinks>
-                            </NavItem>
-                        </NavMenu>
-                        <NavBtn>
-                            <NavBtnLink to="/contact">Nous Contacter</NavBtnLink>
-                        </NavBtn>
-                    </NavbarContainer>
-                </Nav>
-            </IconContext.Provider>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
